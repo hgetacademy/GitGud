3cbf5a8d156777895e8a303ec7419c09b4f900d3
hanlos <hanlos@STUDENT-P00715>
2017-10-17 15:44:30 +0200

    asd

--- GitGud/Program.cs
+++ GitGud/Program.cs
@@ -10,7 +10,7 @@ namespace GitGud
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git Guddness!");
+            Console.WriteLine("Git Guddnesses!");
         }
     }
 }
